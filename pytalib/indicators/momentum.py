462f006d54c10b9eb9674a35ee5cdced6eaf9820
Dennis Cheung <wcgnuehc@yahoo.com.hk>
2018-10-02 13:25:18 +0800

    update
    add momentum indicators

--- pytalib/indicators/momentum.py
+++ pytalib/indicators/momentum.py
@@ -1,5 +1,5 @@
 from .base import MomentumIndicator, AbstractIndicator
-from .trend import SimpleMovingAverage
+from .trend import SimpleMovingAverage, ExponentialMovingAverage
 
 class RateOfChange(MomentumIndicator):
 
@@ -235,11 +235,14 @@ class MoneyFlowIndex(AbstractIndicator):
 		if self.volume is None or len(self.volume) == 0:
 			self.messages.append("`volume` cannot be None or empty.")
 
+		if len(self.prices) != len(self.high) and len(self.high) != len(self.low) and len(self.low) != len(self.volume):
+			self.messages.append("`prices`, `high`, `low`, `volume` must have the same length.")
+
 		if self.period is None or self.period <= 0:
 			self.messages.append("`period` cannot be None.")
 
-		if self.period > len(self.prices) or self.period > len(self.high) or self.period > len(self.low):
-			self.messages.append("`period` cannot be greater than length of `prices`, `high` and `low`.")
+		if self.period > len(self.prices) or self.period > len(self.high) or self.period > len(self.low) or self.period > len(self.volume):
+			self.messages.append("`period` cannot be greater than length of `prices`, `high`, `low` and `volume`.")
 
 		if len(self.messages) > 0:
 			raise Exception(", ".join(self.messages))
@@ -323,39 +326,385 @@ class MoneyFlowIndex(AbstractIndicator):
 
 		return self.mfi
 
-class TrueStrengthIndex(MomentumIndicator):
-	
-	def reset(self, prices):
-		pass
-	
+class TrueStrengthIndex(AbstractIndicator):
+
+	def __init__(self, prices=[], r_period=25, s_period=13):
+		self.r_period = r_period
+		self.s_period = s_period
+		self.momentum = []
+		self.abs_momentum = []
+		self.tsi = []
+		super().__init__(prices)
+
+	def reset(self, prices=[], r_period=25, s_period=13):
+		self.prices = prices
+		self.r_period = r_period
+		self.s_period = s_period
+		self.momentum = []
+		self.abs_momentum = []
+		self.tsi = []
+
+	def validate(self):
+		self._validate()
+
+		if self.r_period is None or self.r_period <= 0:
+			self.messages.append("`r_period` cannot be None.")
+
+		if self.s_period is None or self.s_period <= 0:
+			self.messages.append("`s_period` cannot be None.")
+
+		if self.r_period > len(self.prices):
+			self.messages.append("`r_period` cannot be greater than length of `prices`, `high` and `low`.")
+
+		if self.s_period > len(self.prices):
+			self.messages.append("`s_period` cannot be greater than length of `prices`, `high` and `low`.")
+
+		if len(self.messages) > 0:
+			raise Exception(", ".join(self.messages))
+
+	def get_momentums(self):
+		if len(self.momentum) != 0 and len(self.abs_momentum) != 0:
+			return (self.momentum, self.abs_momentum)
+
+		momentum = []
+		abs_momentum = []
+		for i in range(len(self.prices)):
+			if i == 0:
+				momentum.append(0)
+				abs_momentum.append(0)
+			else:
+				momentum.append(self.prices[i] - self.prices[i - 1])
+				abs_momentum.append(abs(self.prices[i] - self.prices[i - 1]))
+
+		return (self.momentum, self.abs_momentum)
+
 	def calculate(self):
+		if len(self.tsi) != 0:
+			return self.tsi
+
+		momentum, abs_momentum = self.get_momentums()
+
+		ema = ExponentialMovingAverage(momentum, r_period)
+		momentum_ema = ema.calculate()
+		ema.reset(momentum_ema, s_period)
+		smoothed_momentum_ema = ema.calculate()
+
+		ema.reset(abs_momentum_ema, r_period)
+		abs_momentum_ema = ema.calculate()
+		ema.reset(abs_momentum_ema, s_period)
+		smoothed_abs_momentum_ema = ema.calculate()
+
+		for i in range(len(smoothed_momentum_ema)):
+			if i == 0:
+				self.tsi.append(0.00)
+			else:
+				self.tsi.append(round(100 * (smoothed_momentum_ema[i] / smoothed_abs_momentum_ema[i]), 2))
+
+		return self.tsi
+
+class UltimateOscillator(AbstractIndicator):
+	
+	def __init__(self, prices=[], high=[], low=[], s_period=7, m_period=14, l_period=28, s_weight=4, m_weight=2, l_weight=1):
+		self.high = high
+		self.low = low
+		self.s_period = s_period
+		self.m_period = m_period
+		self.l_period = l_period
+		self.s_weight = s_weight
+		self.m_weight = m_weight
+		self.l_weight = l_weight
+		self.bp = []
+		self.tr = []
+		self.uo = []
+		super().__init__(prices)
+
+	def reset(self, prices=[], high=[], low=[], s_period=7, m_period=14, l_period=28, s_weight=4, m_weight=2, l_weight=1):
+		self.prices = prices
+		self.high = high
+		self.low = low
+		self.s_period = s_period
+		self.m_period = m_period
+		self.l_period = l_period
+		self.s_weight = s_weight
+		self.m_weight = m_weight
+		self.l_weight = l_weight
+		self.bp = []
+		self.tr = []
+		self.uo = []
+
+	def validate(self):
+		self._validate()
+
+		if self.high is None or len(self.high) == 0:
+			self.messages.append("`high` cannot be None or empty.")
+
+		if self.low is None or len(self.low) == 0:
+			self.messages.append("`low` cannot be None or empty.")
+
+		if len(self.prices) != len(self.high) and len(self.high) != len(self.low):
+			self.messages.append("`prices`, `high`, `low` must have the same length.")
+
+		if self.s_period is None or self.s_period <= 0:
+			self.messages.append("`s_period` cannot be None.")
+
+		if self.s_period > len(self.prices) or self.s_period > len(self.high) or self.s_period > len(self.low):
+			self.messages.append("`s_period` cannot be greater than length of `prices`, `high` and `low`.")
+
+		if self.m_period is None or self.m_period <= 0:
+			self.messages.append("`m_period` cannot be None.")
+
+		if self.m_period > len(self.prices) or self.m_period > len(self.high) or self.m_period > len(self.low):
+			self.messages.append("`m_period` cannot be greater than length of `prices`, `high` and `low`.")
+
+		if self.l_period is None or self.l_period <= 0:
+			self.messages.append("`l_period` cannot be None.")
+
+		if self.l_period > len(self.prices) or self.l_period > len(self.high) or self.l_period > len(self.low):
+			self.messages.append("`l_period` cannot be greater than length of `prices`, `high` and `low`.")
+
+		if self.s_weight is None or self.s_weight <= 0:
+			self.messages.append("`s_weight` cannot be None.")
+
+		if self.m_weight is None or self.m_weight <= 0:
+			self.messages.append("`m_weight` cannot be None.")
+
+		if self.l_weight is None or self.l_weight <= 0:
+			self.messages.append("`l_weight` cannot be None.")
+
+		if len(self.messages) > 0:
+			raise Exception(", ".join(self.messages))
+
+	def get_bp(self):
+		if len(self.bp) != 0:
+			return self.bp
+
+		for i in range(len(self.prices)):
+			if i == 0:
+				self.bp.append(0.00)
+			else:
+				self.bp.append(round(self.prices[i] - min(self.low[i], self.prices[i - 1]), 2))
 		
-		return 0
+		return self.bp
 
-class UltimateOscillator(MomentumIndicator):
-	
-	def reset(self, prices):
-		pass
-	
-	def calculate(self):
+	def get_tr(self):
+		if len(self.tr) != 0:
+			return self.tr
+
+		for i in range(len(self.prices)):
+			if i == 0:
+				self.tr.append(0.00)
+			else:
+				self.tr.append(round(max(self.high[i], self.prices[i - 1]) - min(self.low[i], self.prices[i - 1]), 2))
 		
-		return 0
+		return self.tr
 
-class Williams(MomentumIndicator):
-	
-	def reset(self, prices):
-		pass
-	
+	def get_period_avg(self, period):
+		period_avg = []
+		bp = self.get_bp()
+		tr = self.get_tr()
+		for i in range(len(self.prices)):
+			if i < period:
+				period_avg.append(0.00)
+			else:
+				period_avg.append(round(sum(bp[i - period + 1 : i + 1]) / sum(tr[i - period + 1 : i + 1]), 2))
+
 	def calculate(self):
+		if len(self.uo) != 0:
+			return self.uo
+
+		s_period_avg = self.get_period_avg(self.s_period)
+		m_period_avg = self.get_period_avg(self.m_period)
+		l_period_avg = self.get_period_avg(self.l_period)
+
+		for i in range(len(self.prices)):
+			if i < self.l_period:
+				self.uo.append(0.00)
+			else:
+				self.uo.append(round( 100 * ((self.s_weight * s_period_avg[i]) + (self.m_weight * m_period_avg[i]) + (self.l_weight * l_period_avg[i])) / (s_weight + m_weight + l_weight),2))
+
+		return self.uo
+
+class Williams(AbstractIndicator):
+	
+	def __init__(self, prices=[], high=[], low=[], period=14):
+		self.high = high
+		self.low = low
+		self.period = period
+		self.williams = []
+		super().__init__(prices)
+
+	def reset(self, prices=[], high=[], low=[], period=14):
+		self.prices = prices
+		self.high = high
+		self.low = low
+		self.period = period
+		self.williams = []
+
+	def validate(self):
+		self._validate()
+
+		if self.high is None or len(self.high) == 0:
+			self.messages.append("`high` cannot be None or empty.")
+
+		if self.low is None or len(self.low) == 0:
+			self.messages.append("`low` cannot be None or empty.")
+
+		if self.period is None or self.period <= 0:
+			self.messages.append("`period` cannot be None.")
+
+		if len(self.prices) != len(self.high) and len(self.high) != len(self.low):
+			self.messages.append("`prices`, `high`, `low` must have the same length.")
+
+		if self.period > len(self.prices) or self.period > len(self.high) or self.period > len(self.low):
+			self.messages.append("`period` cannot be greater than length of `prices`, `high` and `low`.")
+
+		if len(self.messages) > 0:
+			raise Exception(", ".join(self.messages))
+
+	def calculate(self):
+		if len(self.williams) != 0:
+			return self.williams
+
+		for i in range(len(self.prices)):
+			if i < self.period - 1:
+				self.williams.append(0.00)
+			else:
+				hh = max(self.high[i - self.period + 1 : i + 1])
+				ll = min(self.low[i - self.period + 1 : i + 1])
+				self.williams.append(round( -100 * ((hh - self.prices[i]) / (hh - ll)),2))
 		
-		return 0
+		return self.williams
 
-class KnowSureThingOscillator(MomentumIndicator):
-	
-	def reset(self, prices):
-		pass
+class KnowSureThingOscillator(AbstractIndicator):
+
+	def __init__(self, prices=[], ss_roc_period=10, s_roc_period=15, m_roc_period=20, l_roc_period=30, ss_ma_period=10
+			, s_ma_period=10, m_ma_period=10, l_ma_period=15, ss_weight=1, s_weight=2, m_weight=3, l_weight=4, signal_period=9):
+		self.ss_roc_period = ss_roc_period
+		self.s_roc_period = s_roc_period
+		self.m_roc_period = m_roc_period
+		self.l_roc_period = l_roc_period
+		self.ss_ma_period = ss_ma_period
+		self.s_ma_period = s_ma_period
+		self.m_ma_period = m_ma_period
+		self.l_ma_period = l_ma_period
+		self.ss_weight = ss_weight
+		self.s_weight = s_weight
+		self.m_weight = m_weight
+		self.l_weight = l_weight
+		self.signal_period = signal_period
+		self.kst = []
+		self.kst_signal = []
+		super().__init__(prices)
+
+	def reset(self, prices=[], ss_roc_period=10, s_roc_period=15, m_roc_period=20, l_roc_period=30, ss_ma_period=10, s_ma_period=10
+			, m_ma_period=10, l_ma_period=15, ss_weight=1, s_weight=2, m_weight=3, l_weight=4, signal_period=9):
+		self.prices = prices
+		self.ss_roc_period = ss_roc_period
+		self.s_roc_period = s_roc_period
+		self.m_roc_period = m_roc_period
+		self.l_roc_period = l_roc_period
+		self.ss_ma_period = ss_ma_period
+		self.s_ma_period = s_ma_period
+		self.m_ma_period = m_ma_period
+		self.l_ma_period = l_ma_period
+		self.ss_weight = ss_weight
+		self.s_weight = s_weight
+		self.m_weight = m_weight
+		self.l_weight = l_weight
+		self.signal_period = signal_period
+		self.kst = []
+		self.kst_signal = []
+
+	def validate(self):
+		self._validate()
+
+		if self.ss_roc_period is None or self.ss_roc_period <= 0:
+			self.messages.append("`ss_roc_period` cannot be None.")
+
+		if self.s_roc_period is None or self.s_roc_period <= 0:
+			self.messages.append("`s_roc_period` cannot be None.")
+
+		if self.m_roc_period is None or self.m_roc_period <= 0:
+			self.messages.append("`m_roc_period` cannot be None.")
+
+		if self.l_roc_period is None or self.l_roc_period <= 0:
+			self.messages.append("`l_roc_period` cannot be None.")
+
+		if self.ss_ma_period is None or self.ss_ma_period <= 0:
+			self.messages.append("`ss_ma_period` cannot be None.")
+
+		if self.s_ma_period is None or self.s_ma_period <= 0:
+			self.messages.append("`s_ma_period` cannot be None.")
+
+		if self.m_ma_period is None or self.m_ma_period <= 0:
+			self.messages.append("`m_ma_period` cannot be None.")
+
+		if self.l_ma_period is None or self.l_ma_period <= 0:
+			self.messages.append("`l_ma_period` cannot be None.")
+
+		if self.ss_weight is None or self.ss_weight <= 0:
+			self.messages.append("`ss_weight` cannot be None.")
+
+		if self.s_weight is None or self.s_weight <= 0:
+			self.messages.append("`s_weight` cannot be None.")
+
+		if self.m_weight is None or self.m_weight <= 0:
+			self.messages.append("`m_weight` cannot be None.")
+
+		if self.l_weight is None or self.l_weight <= 0:
+			self.messages.append("`l_weight` cannot be None.")
+
+		if self.signal_period is None or self.signal_period <= 0:
+			self.messages.append("`signal_period` cannot be None.")
+
+		if self.ss_roc_period > self.s_roc_period and self.s_roc_period > self.m_roc_period and self.m_roc_period > self.l_roc_period:
+			self.messages.append("Invalid: `ss_roc_period` > `s_roc_period` and `s_roc_period` > `m_roc_period` and `m_roc_period` > `l_roc_period`")
+
+		if self.ss_ma_period > self.s_ma_period and self.s_ma_period > self.m_ma_period and self.m_ma_period > self.l_ma_period:
+			self.messages.append("Invalid: `ss_ma_period` > `s_ma_period` and `s_ma_period` > `m_ma_period` and `m_ma_period` > `l_ma_period`")
+
+		if self.ss_roc_period > len(self.prices):
+			self.messages.append("`ss_roc_period` cannot be greater than length of `prices`.")
+
+		if self.ss_ma_period > len(self.prices):
+			self.messages.append("`ss_ma_period` cannot be greater than length of `prices`.")
+
+		if len(self.messages) > 0:
+			raise Exception(", ".join(self.messages))
 
 	def calculate(self):
+		if len(self.kst) != 0 and len(self.kst_signal) != 0:
+			return (self.kst, self.kst_signal)
+
 		self.validate()
-		
-		return 0
+
+		roc = RateOfChange(self.prices, ss_roc_period)
+		ss_roc = roc.calculate()
+		sma = SimpleMovingAverage(ss_roc, self.ss_ma_period)
+		ss_ma = sma.calculate()
+
+		roc.reset(self.prices, s_roc_period)
+		s_roc = roc.calculate()
+		sma.reset(s_roc, self.s_ma_period)
+		s_ma = sma.calculate()
+
+		roc.reset(self.prices, m_roc_period)
+		m_roc = roc.calculate()
+		sma.reset(s_roc, self.m_ma_period)
+		m_ma = sma.calculate()
+
+		roc.reset(self.prices, l_roc_period)
+		l_roc = roc.calculate()
+		sma.reset(s_roc, self.l_ma_period)
+		l_ma = sma.calculate()
+
+		for i in range(len(self.prices)):
+			if i < self.l_roc_period + self.l_ma_period - 1:
+				self.kst.append(0.00)
+			else:
+				self.kst.append(round((ss_ma[i] * self.ss_weight) + (s_ma[i] * self.s_weight) + (m_ma[i] * self.m_weight) + (l_ma[i] * self.l_weight), 2))
+
+		sma.reset(self.kst, self.signal_period)
+		self.kst_signal = sma.calculate()
+
+		return (self.kst, self.kst_signal)
